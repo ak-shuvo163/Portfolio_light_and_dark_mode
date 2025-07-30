2221043af7e9d8150c5f17988b5c1600b5587567
AK Shuvo <akshuvo163@gmail.com>
2025-07-31 03:04:13 +0600

    change something

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  // content: ["./src/**/*.{html,js}"],
+  content: ["./dist/index.html", "./src/**/*.{html,js}"],
   safelist: ['right-[-100%]', 'right-0'],
   darkMode: 'class',
   theme: {
